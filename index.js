23819f43d08f48fbee8815c25940d881b31c67cd
KarlMarkusVahe <113974010+KarlMarkusVahe@users.noreply.github.com>
2023-02-06 16:29:51 +0200

    Added static css and image files

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const hbs = require('express-handlebars')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
-app.engine('hbs', hbs.engine ({
+app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: __dirname + '/views/layouts/'
+    layoutsDir: __dirname + '/views/layouts/',
 }))
 
+app.use(express.static('public'))
+
 const mysql = require('mysql')
+
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: true}))
